71b4454cfa0855ce87ea8b823460664768f625e7
Ivan Donyk <ivandonyk@gmail.com>
2021-05-12 17:41:24 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "accuranker",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "gNAz5s3S6bcgnPLbEQ9Mwtlg5z-kABTlmGQnUf-d0pk",
+        spaceId: "tdpyozytknjz",
+      },
+    },
+  ],
+};
