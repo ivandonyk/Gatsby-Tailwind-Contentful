bd2b71ffbc0820089738bc99bcd9caf96d8b5fa5
Ivan Donyk <ivandonyk@gmail.com>
2021-05-14 09:45:58 -0400

    Add dynamic header and connectitems from Contentful

--- src/components/HomepageHeader.tsx
+++ src/components/HomepageHeader.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import BookDemo from "./BookDemo";
+import { ContentfulHomePageHeaderJsonNode } from "../../graphql-types";
 
-interface headerProps {}
+interface headerProps {
+  title: string;
+  subtitle: string;
+}
 
-const Header: React.FC<headerProps> = ({}) => {
+const Header: React.FC<headerProps> = ({ title, subtitle }) => {
+  const titleWithStyle = title.split(" ").map((str, i) => {
+    if (i === 1) return <span className="text-ar-fountain-blue">{str} </span>;
+    else return `${str} `;
+  });
   return (
     <div className="">
       <div className="text-5xl pt-16 font-semibold text-center">
-        SERP <span className="text-ar-fountain-blue">Analytics</span> For Search
-        Professionals
+        {titleWithStyle.map((str) => str)}
       </div>
-      <div className="text-3xl font-light text-center pt-4">
-        Accurate position and SERP analytics
-      </div>
+      <div className="text-3xl font-light text-center pt-4">{subtitle}</div>
       <div className="pt-10 md:w-1/2 mx-auto">
         <BookDemo />
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,46 +8,36 @@ import { ContentfulHomePage } from "../../graphql-types";
 
 import Connections1 from "../images/connections-1.svg"; // todo: fix this,it works but typescript doesn't like it
 
-export const IndexPage: React.FC<PageProps<ContentfulHomePage>> = ({
-  data,
-}) => {
+type ContentfulHomePageProps = {
+  contentfulHomePage: ContentfulHomePage;
+};
+
+type IndexPageProps = {
+  data: ContentfulHomePageProps;
+} & PageProps;
+
+export const IndexPage: React.FC<IndexPageProps> = ({ data }) => {
+  const header = data.contentfulHomePage.header;
+  const colors = ["picton-blue", "turquoise", "mantis", "wattle"];
+
   return (
     <main className="bg-ar-dark">
       <section className="container mx-auto text-white px-4">
         <Navbar />
-        <HomepageHeader />
+        <HomepageHeader title={header.title} subtitle={header.subitle} />
         <div className="relative ">
           <img src={Connections1} className="w-full" />
           <div className="absolute top-0 w-11/12 md:pt-8 lg:pt-12 xl:pt-16 2xl:pt-24  connection-1-texts">
             <div className="flex justify-center">
-              <div className="w-1/4">
-                <ConnectionItem
-                  color="picton-blue"
-                  text="KEYWORDS TRACKED"
-                  number="12,568,100"
-                />
-              </div>
-              <div className="w-1/4">
-                <ConnectionItem
-                  color="turquoise"
-                  text="Keyword database"
-                  number="14,560,156,563"
-                />
-              </div>
-              <div className="w-1/4">
-                <ConnectionItem
-                  color="mantis"
-                  text="Pixels tracked"
-                  number="29,568,159,548"
-                />
-              </div>
-              <div className="w-1/4">
-                <ConnectionItem
-                  color="wattle"
-                  text="Data processed"
-                  number="568,569 TB"
-                />
-              </div>
+              {data.contentfulHomePage.connectionItems.map((item, i) => (
+                <div className="w-1/4">
+                  <ConnectionItem
+                    color={colors[i]}
+                    text={item.text}
+                    number={item.number}
+                  />
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -57,7 +47,7 @@ export const IndexPage: React.FC<PageProps<ContentfulHomePage>> = ({
 };
 
 type ConnectionItemProps = {
-  color: "picton-blue" | "turquoise" | "mantis" | "wattle";
+  color: string;
   text: string;
   number: string;
 };
